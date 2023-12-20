3ea923ee6bcce2f603328f1bda4f95e3666f7e90
bellegry <gayoung.work@gmail.com>
2023-12-20 16:43:46 +0900

    checked

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
+// todoList
 const todoList = document.querySelector(".list")
-const plusBtn = document.querySelector(".search button")
+let todoTextList = []
+let todoMemoList = []
 
-plusBtn.addEventListener("click", () => {
-  const todoText = document.querySelector(".search input").value
+function todoPlus() {
+  let todoText = document.querySelector(".search input").value
   const temp = document.createElement("div")
   
-  temp.innerHTML = `<input type="checkbox" class="todoMemo" name="${todoText}" id="todoMemo[]" onclick="is_checked()" />${todoText}<button class="deleteBtn">삭제</button>`
+  temp.innerHTML = `<input type="checkbox" class="todoMemo" name="${todoText}" id="todoMemo" onclick="is_checked()" />${todoText}<button class="deleteBtn" onclick="deleteBtn()">삭제</button>`
 
   todoList.append(temp)
-  
-  // check box
-  function is_checked() {
-    const todoMemoList = document.querySelectorAll(".todoMemo")
-    
-    for (const todoMemoList of todoMemo) {
-      if(todoMemo.checked) {
-        console.log("Asdf")
-      }
+  todoTextList.push(temp)
+}
+
+// checked
+function is_checked() {
+  todoMemoList = document.querySelectorAll(".todoMemo") 
+  todoMemoList.forEach((el) => {
+    if(el.checked) {
+      el.parentElement.classList.add("on")
+    } else {
+      el.parentElement.classList.remove("on")
     }
-  }
-})
+  });
+}
+
+// delete
+function deleteBtn() {
+  todoMemoList = document.querySelectorAll(".todoMemo") 
+  todoMemoList.forEach((el) => {
+    if(el.checked) {
+      el.parentElement.classList.add("on")
+    } else {
+      el.parentElement.classList.remove("on")
+    }
+  });
+}
+
 
